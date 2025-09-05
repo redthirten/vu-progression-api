bb798dd9b6595e857e032115d6bdc38f287d872e
Red-Thirten <lilkingjr13@yahoo.com>
2025-09-05 00:48:55 -0700

    Add Min. Mod Ver. Supported to Index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,12 @@
 import express from "express";
 import { createRequire } from "module";
 
+const MIN_MOD_VERSION = {
+    Major: 2,
+    Minor: 1,
+    Patch: 1
+};
+
 // Get basic app info
 const require = createRequire(import.meta.url);
 const {
@@ -23,6 +29,11 @@ rootRouter.get("/", (req, res) => {
         name: appName,
         description: appDesc,
         version: appVer,
-        github: appRepo.url
+        github: appRepo.url,
+        minModVerSupported: {
+            Major: MIN_MOD_VERSION.Major,
+            Minor: MIN_MOD_VERSION.Minor,
+            Patch: MIN_MOD_VERSION.Patch
+        }
     });
 });
